f3d555dbba257169e35078da3dd34f0566ce21b4
YTK <yu.nakkano@gmail.com>
2020-09-08 12:18:27 +0900

    Customized portfolio_show path

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :portfolios
+  resources :portfolios, except: [:show]
+  get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
+
   # These are 'static' pages which doesn't need resources
   get 'about', to: 'pages#about'
   get 'contact', to: 'pages#contact'
